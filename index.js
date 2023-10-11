1fe0eee12089141ef529bcbb0dcc0c00b99b32b7
Marcin Krawczyk <pinfix01orginal@gmail.com>
2023-10-11 14:01:09 +0200

    :3

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ async function listDB(client) {
     let databaseList = await client.db().admin().listDatabases();
 
     console.log(databaseList);
+
+    databaseList.databases.forEach(database => {
+        console.log("Nazwa: " + database.name + ", Rozmiar: " + database.sizeOnDisk);
+    })
 }
 
 main();
