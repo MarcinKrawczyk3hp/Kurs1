5f35cc482c79aa98b233f0f630533985ab450c1c
Marcin Krawczyk <pinfix01orginal@gmail.com>
2023-10-11 13:37:28 +0200

    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoString = "mongodb+srv://marcin240204:Rademenes13@cluster0.dat668z.mongodb.net/?retryWrites=true&w=majority";
 const { MongoClient } = require('mongodb');
 
-const client  = MongoClient(mongoString);
+const client = new MongoClient(mongoString);
 
 async function main() {
     await client.connect();
